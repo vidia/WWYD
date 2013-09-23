7361d19d9da9ef5b892d970b738834f79f4b3038
David Tschida <dmtschida1@gmail.com>
2013-09-23 16:55:45 -0400

    Begin creation of a class to represent entries into the app. It will save all parts of the entries like verb, item, description, etc.

---
+++ src/tschida/david/wwyd/Entry.java
@@ -0,0 +1,26 @@
+package tschida.david.wwyd;
+
+public class Entry
+{
+	String verb, thing, description, friends;
+	
+	public Entry(String verb, String thing, String description, String friends)
+	{
+		
+	}
+	
+	public Entry()
+	{
+		
+	}
+	
+	public void printToFile()
+	{
+		
+	}
+	
+	public Entry addVerb()
+	{
+		return null;
+	}
+}
